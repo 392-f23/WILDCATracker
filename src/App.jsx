e8ad61095f21268750d47db23e638cf956c8557e
benmagevney <86267952+benmagevney@users.noreply.github.com>
2023-10-05 17:19:16 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import games_data from './utilities/data.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const Main = () => {
+  console.log(games_data);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+
+  );
+};
+
+const App = () => {
+
+
+  return (
+    <Main>
+
+    </Main>
   );
 };
 
