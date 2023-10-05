c2557c35d9a7519144370ed15ac0b2d7dd0b7665
Jadon Lau <jadon@Jadons-MacBook-Air.local>
2023-10-05 18:29:22 -0500

    Jadon -- Added game card structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,10 +9,10 @@ const NavBar = () => {
 	};
 
 	return (
-		<nav class='navbar navbar-expand-lg navbar-light bg-light'>
-			<div class='container-fluid'>
+		<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+			<div className='container-fluid'>
 				<button
-					class='navbar-toggler'
+					className='navbar-toggler'
 					type='button'
 					data-bs-toggle='collapse'
 					data-bs-target='#navbarNav'
@@ -20,22 +20,22 @@ const NavBar = () => {
 					aria-expanded='false'
 					aria-label='Toggle navigation'
 				>
-					<span class='navbar-toggler-icon'></span>
+					<span className='navbar-toggler-icon'></span>
 				</button>
-				<div class='collapse navbar-collapse' id='navbarNav'>
-					<ul class='navbar-nav'>
-						<li class='nav-item'>
-							<a class='nav-link active' aria-current='page' href='#' onClick={() => navigate_to("/games")}>
+				<div className='collapse navbar-collapse' id='navbarNav'>
+					<ul className='navbar-nav'>
+						<li className='nav-item'>
+							<a className='nav-link active' aria-current='page' href='#' onClick={() => navigate_to("/games")}>
 								Games
 							</a>
 						</li>
-						<li class='nav-item'>
-							<a class='nav-link' href='#'>
+						<li className='nav-item'>
+							<a className='nav-link' href='#' onClick={() => navigate_to("/points")}>
 								Points
 							</a>
 						</li>
-						<li class='nav-item'>
-							<a class='nav-link' href='#'>
+						<li className='nav-item'>
+							<a className='nav-link' href='#' onClick={() => navigate_to("/home")}>
 								Home
 							</a>
 						</li>
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import NavBar from "./NavBar"
+import GameCard from "./GameCard"
+import games_data from "../utilities/data.js";
 
 const MyRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element = {
+					<div>
+						<NavBar></NavBar>
+						<GameCard Game={games_data["1"]}></GameCard>
+					</div>
+				}></Route>
+				<Route path='/games' element = {
 					<NavBar></NavBar>
 				}></Route>
-				<Route path='/games'></Route>
-				<Route path='/points'></Route>
-				<Route path='/home'></Route>
+				<Route path='/points' element = {
+					<NavBar></NavBar>
+				}></Route>
+				<Route path='/home' element = {
+					<NavBar></NavBar>
+				}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
