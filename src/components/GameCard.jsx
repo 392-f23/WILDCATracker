02db54ed31fe5b768e99b8abea65ae9dd8e90c5f
Jadon Lau <jadon@Jadons-MacBook-Air.local>
2023-10-06 22:21:17 -0500

    Jadon -- formatted game cards, button point action, added few filters

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,24 +1,39 @@
+import React, { useState } from 'react';
+
 const GameCard = ({ game }) => {
+    const [buttonText, setButtonText] = useState('Points ' + game.points);
+    const [buttonColor, setButtonColor] = useState('btn-primary');
+
+    const handleButtonClick = () => {
+        if (buttonText === 'Points ' + game.points) {
+            setButtonText('Points Added');
+            setButtonColor('btn-success');
+        } else {
+            setButtonText('Points ' + game.points);
+            setButtonColor('btn-primary');
+        }
+    };    
 	return (
 		<div className='card m-2 p-2'>
-			<div className='card-body'>
-				<h5 className='card-title'>{game.sport}</h5>
-				<h6 className='card-subtitle mb-2 text-muted'>
-					Northwestern vs {game.opponent}
-				</h6>
-				<h6 className='card-subtitle mb-2 text-muted'>
-					{new Date(game.date).toDateString()} {game.time}
-				</h6>
-				<h6 className='card-subtitle mb-2 text-muted'>{game.location}</h6>
-				<p className='card-text'>
-					Some quick example text to build on the card title and make up the
-					bulk of the card's content.
-				</p>
-				<a href='#' className='btn btn-primary'>
-					Points {game.points}
-				</a>
-			</div>
-		</div>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%", padding: "5px" }}>
+                <div>
+                    <h5 className='card-title'>{game.sport}</h5>
+                    <h6 className='card-subtitle mb-2 text-muted'>
+                        Northwestern vs {game.opponent}
+                    </h6>
+                    <h6 className='card-subtitle mb-2 text-muted'>
+                        {new Date(game.date).toDateString()} {game.time}
+                    </h6>
+                </div>
+                <div>
+                    <hr></hr>
+                    <h6 className='card-subtitle mb-2 text-muted'>{game.location}</h6>
+                    <a style={{ width: "100%" }}className={`btn btn-primary btn-block ${buttonColor}`} onClick={handleButtonClick}>
+                        {buttonText}
+                    </a>
+                </div>
+        </div>
+    </div>
 	);
 };
 
