505c14ab68245ca35547610b29269a43ebbb3bb1
benmagevney <86267952+benmagevney@users.noreply.github.com>
2023-10-09 14:43:27 -0500

    make chart work and filter past future games

--- src/AttendedGamesChart.jsx
+++ src/AttendedGamesChart.jsx
@@ -3,35 +3,83 @@ import { useEffect, useRef } from "react";
 import { get_date } from "./utilities/get_date";
 import { historyLookback } from "./components/PointsPage";
 
+const formatDateToMDY = (date) => {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`;
+};
+
+const generateEquallySpreadDates = (startDate, endDate, n) => {
+    const dateList = [];
+    const interval = (endDate - startDate) / (n - 1);
+
+    for (let i = 0; i < n; i++) {
+        const newDate = new Date(startDate.getTime() + interval * i);
+        dateList.push(newDate);
+    }
+    return dateList;
+};
+
+const getFirstDate = (games, window) => {
+    window == "Total" ? new Date(games.reduce((a, b) => new Date(a.date) < new Date(b.date) ? a : b).date) : historyLookback[window];
+    if (window === "Total") {
+        const date = new Date(games.reduce((a, b) => new Date(a.date) < new Date(b.date) ? a : b).date);
+        return new Date(date.setDate(date.getDate() - 1));
+    };
+    return historyLookback[window];
+};
+
 const AttendedGamesChart = ({ games, window }) => {
-	const chartRef = useRef(null);
+    const chartRef = useRef(null);
 
-	games = games.map((game) => {
-		return {
-			...game,
-			date: get_date(new Date(game.date)),
-		};
-	});
+    games = games.map((game) => {
+        return {
+            ...game,
+            date: get_date(new Date(game.date)),
+        };
+    });
 
-	useEffect(() => {
-		const context = chartRef.current.getContext("2d");
-		const chart = new Chart(context, {
-			type: "line",
-			data: {
-				labels: games.map((game) => game.date),
-				datasets: [
-					{
-						data: games.map((game) => game.points),
-						borderColor: "#4E2A84",
-						backgroundColor: "#4E2A84",
-					},
-				],
-			},
-		});
-		return () => chart.destroy();
-	}, []);
+    const firstDate = getFirstDate(games, window);
 
-	return <canvas ref={chartRef}></canvas>;
+    const dateLabels = generateEquallySpreadDates(firstDate, new Date(), 7);
+    const points = [games.filter((game) => new Date(game.date) <= firstDate).reduce((sum, game) => sum + game.points, 0)];
+    for (let i = 1; i < dateLabels.length; i++) {
+        const newPoints = games.filter((game) => dateLabels[i - 1] < new Date(game.date) <= dateLabels[i]).reduce((sum, game) => sum + game.points, 0);
+        points.push(newPoints);
+    };
+    useEffect(() => {
+        const context = chartRef.current.getContext("2d");
+        const chart = new Chart(context, {
+            type: "line",
+            data: {
+                labels: dateLabels.map((date) => formatDateToMDY(date)),
+                datasets: [
+                    {
+                        data: points,
+                        borderColor: "#4E2A84",
+                        backgroundColor: "#4E2A84",
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false, // Hide the legend
+                    },
+                },
+            },
+        });
+        return () => chart.destroy();
+    }, [window]);
+
+    return <canvas ref={chartRef}></canvas>;
 };
 
 export default AttendedGamesChart;
--- src/components/PointsPage.jsx
+++ src/components/PointsPage.jsx
@@ -10,17 +10,29 @@ const historyWindows = ["Total", "Week", "Month"];
 const today = new Date();
 export const historyLookback = {
 	Total: new Date(0),
-	Week: new Date(today).setDate(today.getDate() - 7),
-	Month: new Date(today).setDate(today.getDate() - 30),
+	Week: new Date(new Date(today).setDate(today.getDate() - 7)),
+	Month: new Date(new Date(today).setDate(today.getDate() - 30)),
 };
 
-const PointsPage = ({ user }) => {
+const PointsPage = () => {
+	if (!localStorage.getItem("games_attended")) {
+		localStorage.setItem(
+			"games_attended",
+			JSON.stringify(users_data[0].attended_games)
+		);
+	}
+
+	const attendedGameIds = JSON.parse(localStorage.getItem("games_attended")).filter((game) => game != null);
 	const attendedGames = games_data.filter((game) =>
-		user.attended_games.includes(game.id)
+		attendedGameIds.includes(game.id) && new Date() >= new Date(game.date)
+	);
+	const futureGames = games_data.filter((game) =>
+		attendedGameIds.includes(game.id) && new Date() < new Date(game.date)
 	);
 	const points = attendedGames.reduce((sum, game) => sum + game.points, 0);
 	const [filteredGames, setFilteredGames] = useState(attendedGames);
 	const [window, setWindow] = useState("Total");
+	const [past, setPast] = useState(true);
 
 	const filterGames = (filter) => {
 		setWindow(filter);
@@ -30,6 +42,7 @@ const PointsPage = ({ user }) => {
 			)
 		);
 	};
+	console.log(past);
 	return (
 		<div className='content'>
 			<NavBar></NavBar>
@@ -63,8 +76,37 @@ const PointsPage = ({ user }) => {
 					</div>
 				))}
 			</div>
-			<AttendedGamesChart games={filteredGames} window={window} />
-			<AttendedGamesList games={filteredGames} />
+			<AttendedGamesChart games={attendedGames} window={window} />
+			<div
+				className='button-group'
+				style={{ display: "flex", alignItems: "center" }}
+			>
+				{[{ label: "Past", val: true }, { label: "Future", val: false }].map((option) => (
+					<div key={option.label} style={{ margin: "auto" }}>
+						<input
+							type='radio'
+							className='btn-check'
+							name='btnradio'
+							id={option.label}
+							autoComplete='off'
+							defaultChecked={past == option.val}
+							onClick={() => setPast(option.val)}
+						/>
+						<label
+							className={
+								option.val === past
+									? "time-chart-label-selected"
+									: "time-chart-label-unselected"
+							}
+							id={option.label}
+							htmlFor={option.label}
+						>
+							{option.label}
+						</label>
+					</div>
+				))}
+			</div>
+			{past ? <AttendedGamesList games={filteredGames} /> : <AttendedGamesList games={futureGames} />}
 		</div>
 	);
 };
