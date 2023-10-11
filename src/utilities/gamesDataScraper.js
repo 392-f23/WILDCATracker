031bc3a70505905d37d2afbcb132b9d410e127e9
Murat Sankaya <muratsankaya2025@u.northwestern.edu>
2023-10-11 13:43:50 -0500

    testing realtime web interaction

--- src/utilities/gamesDataScraper.js
+++ src/utilities/gamesDataScraper.js
@@ -3,11 +3,18 @@ const puppeteer = require('puppeteer');
 const URL = 'https://nusports.com/calendar';
 
 async function scrapeAndPrint() {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    
+    // This will wait untill all the dynamic data in the page is loaded 
+    await page.goto(URL, { waitUntil: 'networkidle2' });
 
-    // networkidle2 ensures all dynamic data is loaded 
-    await page.goto(URL, { waitUntil: 'networkidle2' });  
+    // Click the "Previous month" button.
+    await page.click('button.fc-prev-button');
+
+    // Will wait until the selector appears or the provided timeout
+    await page.waitForSelector('span.s-text-details-bold.text-theme-safe-light.flex.items-center > span.sr-only', 
+                                { timeout: 10000 });  
 
     const textContent = await page.$$eval(
         'span.s-text-details-bold.text-theme-safe-light.flex.items-center > span.sr-only',
@@ -15,7 +22,7 @@ async function scrapeAndPrint() {
     );
     
     textContent.forEach(text => console.log(text));
-
+    
     await browser.close();
 }
 
